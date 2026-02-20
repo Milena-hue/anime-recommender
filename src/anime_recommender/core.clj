0a4b984491fbffc8f0a379a2792853ccb10de5e7
Milena-hue <milenanikolic.mika@gmail.com>
2026-02-20 16:43:21 +0100

    Add anime database

--- src/anime_recommender/core.clj
+++ src/anime_recommender/core.clj
@@ -1,7 +1,28 @@
 (ns anime-recommender.core
   (:gen-class))
+;; Baza anime podataka
+;; Svaki anime ima id, title, genres, studio i rating
+(def anime-db
+  [{:id 1
+    :title "Attack on Titan"
+    :genres ["Action" "Drama" "Suspense"]
+    :studio "Wit Studio"
+    :rating 8.57}
+   
+   {:id 2
+    :title "Death Note"
+    :genres ["Supernatural" "Suspense"]
+    :studio "Madhouse"
+    :rating 8.62}
+   
+   {:id 3
+    :title "Bleach"
+    :genres ["Action" "Adventure" "Supernatural"]
+    :studio "Studio Pierrot"
+    :rating 7.99}])
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Anime Recommender - entry point"
   [& args]
-  (println "Hello, World!"))
+  (println "Dobrodošli u Anime Recommeder!")
+  (println "Broj animea u bazi:" (count anime-db)))
