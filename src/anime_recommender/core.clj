aacaccc2099676e2112ef99bf972624268f4a69d
Milena-hue <milenanikolic.mika@gmail.com>
2026-02-20 16:29:01 +0100

    Initial commit - starter project

---
+++ src/anime_recommender/core.clj
@@ -0,0 +1,7 @@
+(ns anime-recommender.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/anime_recommender/core_test.clj
@@ -0,0 +1,7 @@
+(ns anime-recommender.core-test
+  (:require [clojure.test :refer :all]
+            [anime-recommender.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
